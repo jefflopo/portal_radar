3e8095b5401e2a5309c88811ba1fcd6aeb992bf6
jefflopo <jeffersonlopo@gmail.com>
2017-03-31 13:08:43 -0300

    Adicionada pasta view para armazenar as paginas

---
+++ view/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Boletim Radar</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="lib/owl.carousel/owl.carousel.css">
+	<link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/folha_estilo_ipea.css">
+    </head>
+    <body>
+        
+        <header>
+			
+            <div class="container">
+                <a href="index.html"><img id="logotipo" src="img/logo_radar.png" alt="Logotipo Radar"></a>
+            </div>
+
+            <div class="header-gray">
+                
+                <a class="brasil" href="http://brasil.gov.br/">Brasil</a>
+                <a class="servicos" href="http://www.servicos.gov.br/?pk_campaign=barrabrasil">Serviços</a>
+                <a class="participe" href="http://brasil.gov.br/barra#participe">Participe</a>
+                <a class="informacao" href="http://brasil.gov.br/barra#acesso-informacao">Acesso à Informação</a>
+                <a class="legislacao" href="http://www.planalto.gov.br/legislacao">Legislação</a>
+                <a class="canais" href="http://brasil.gov.br/barra#orgaos-atuacao-canais">Canais</a>
+                <a class="vlibras" href="http://www.vlibras.gov.br/"><i class="fa fa-sign-language"></i>VLibras</a>
+
+            </div>
+
+            <div class="pull-right">
+
+                <ul id="menu" class="nav nav-pills">
+                    <li role="presentation"><a href="index.html">Início</a></li>
+                    <li role="presentation" class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Temas<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                                <li><a href="agricultura.html">Agricultura</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="ciencia_tecnologia.html">Ciência, Tecnologia e Inovação</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Comércio Exterior</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Educação</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Indústria</a></li>
+                        </ul>
+                    </li>
+                    <li role="presentation"><a href="radarPDF.html">Radar PDF</a></li>
+                    <li role="presentation" class="search">
+                        <div class="input-group">
+                            <input type="search" placeholder="Pesquisar" id="input-search">
+                            <span class="input-group-btn">
+                                <button type="button"><i class="fa fa-search"></i></button>
+                            </span>
+                        </div><!-- /input-group -->
+                    </li>
+                </ul>
+
+            </div>			
+
+	</header>
