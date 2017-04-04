0d3e878c7c3fcf21c17f34b1d01d71f7a5b5b87c
jefflopo <jeffersonlopo@gmail.com>
2017-04-04 12:46:55 -0300

    Todas as paginas com extensao PHP

---
+++ view/radarPDF_.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Boletim Radar</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../lib/owl.carousel/owl.carousel.css">
+	<link rel="stylesheet" href="../fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="../css/folha_estilo_ipea.css">
+    </head>
+    <body>
+        
+        <header>
+			
+            <div class="container">
+                <a href="index.html"><img id="logotipo" src="../img/logo_radar.png" alt="Logotipo Radar"></a>
+            </div>
+
+            <div class="header-gray">
+                
+                <a class="brasil" href="http://brasil.gov.br/">Brasil</a>
+                <a class="servicos" href="http://www.servicos.gov.br/?pk_campaign=barrabrasil">Serviços</a>
+                <a class="participe" href="http://brasil.gov.br/barra#participe">Participe</a>
+                <a class="informacao" href="http://brasil.gov.br/barra#acesso-informacao">Acesso à Informação</a>
+                <a class="legislacao" href="http://www.planalto.gov.br/legislacao">Legislação</a>
+                <a class="canais" href="http://brasil.gov.br/barra#orgaos-atuacao-canais">Canais</a>
+                <a class="vlibras" href="http://www.vlibras.gov.br/"><i class="fa fa-sign-language"></i>VLibras</a>
+
+            </div>
+
+            <div class="pull-right">
+
+                <ul id="menu" class="nav nav-pills">
+                    <li role="presentation"><a href="../index.html">Início</a></li>
+                    <li role="presentation" class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Temas<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                                <li><a href="agricultura.html">Agricultura</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="ciencia_tecnologia.html">Ciência, Tecnologia e Inovação</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Comércio Exterior</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Educação</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Indústria</a></li>
+                        </ul>
+                    </li>
+                    <li role="presentation"><a href="radarPDF.html">Radar PDF</a></li>
+                    <li role="presentation" class="search">
+                        <div class="input-group">
+                            <input type="search" placeholder="Pesquisar" id="input-search">
+                            <span class="input-group-btn">
+                                <button type="button"><i class="fa fa-search"></i></button>
+                            </span>
+                        </div><!-- /input-group -->
+                    </li>
+                </ul>
+
+            </div>			
+
+	</header>
+
+        
+        <section>
+            
+            <div id="banner">
+                <a href="http://www.ipea.gov.br/" target="_blank">
+                    <img src="../img/logo_ipea.png" alt="Logo do IPEA">
+                </a>
+            </div>
+            
+            <div id="edicoes" class="container">
+                
+                <div class="row text-center">
+                    <h2>Edições Completas</h2>
+                    <hr>
+                </div>
+                
+                <div class="row thumbnails">
+                    
+                    <div class="col-md-3">
+                            <h3>Artigo nº 48</h3>
+                            <img src="../img/170124_radar_48.jpg" alt="Artigo nº 48">
+                            <time>2016 - Dezembro</time>
+                        </div>
+                        
+                        <div class="col-md-3">
+                            <h3>Artigo nº 47</h3>
+                            <img src="../img/170124_radar_47.jpg" alt="Artigo nº 47">
+                            <time>2016 - Outubro</time>
+                        </div>
+                        
+                        <div class="col-md-3">
+                            <h3>Artigo nº 46</h3>
+                            <img src="../img/161108_radar_46.jpg" alt="Artigo nº 46">
+                            <time>2016 - Agosto</time>
+                        </div>
+                        
+                        <div class="col-md-3">
+                            <h3>Artigo nº 45</h3>
+                            <img src="../img/160714_radar_45.jpg" alt="Artigo nº 45">
+                            <time>2016 - Junho</time>
+                        </div>
+                    
+                </div>
+                
+                <br><br>
+                
+                <div class="row thumbnails">
+                    
+                    <div class="col-md-3">
+                        <h3>Artigo nº 44</h3>
+                        <img src="../img/160502_radar_44.jpg" alt="Artigo nº 44">
+                        <time>2016 - Abril</time>
+                    </div>
+                    
+                    <div class="col-md-3">
+                        <h3>Artigo nº 43</h3>
+                        <img src="../img/160309_radar_43.jpg" alt="Artigo nº 43">
+                        <time>2016 - Fevereiro</time>
+                    </div>
+                    
+                    <div class="col-md-3">
+                        <h3>Artigo nº 42</h3>
+                        <img src="../img/151228_radar_42.jpg" alt="Artigo nº 42">
+                        <time>2015 - Dezembro</time>
+                    </div>
+                    
+                    <div class="col-md-3">
+                        <h3>Artigo nº 41</h3>
+                        <img src="../img/151106_radar_41.jpg" alt="Artigo nº 41">
+                        <time>2015 - Outubro</time>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+        </section>
+        
+        <footer>
+            
+            <div class="row row-verde_silver_tree">
+                
+                <div class="container">
+                    
+                    <div class="row">
+                        
+                        <div class="col-md-12">
+                            
+                            <div class="col-md-4 col-mais-acessados">
+                                
+                                <h4>Mais Acessados</h4>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <h5>Neque porro quisquam est, quister.</h5>
+                                        <time>January 01, 2016</time>
+                                    </li>
+                                    <li>
+                                        <h5>Neque porro quisquam est, quister.</h5>
+                                        <time>January 01, 2016</time>
+                                    </li>
+                                </ul>
+                                
+                            </div>
+                            
+                            <div class="col-md-4 col-links">
+                                
+                                <h4>LINKS</h4>
+                                <ul class="list-unstyled">
+                                    <li><a href="http://www.ipea.gov.br" target="_blank"><i class=" fa fa-angle-right"></i>IPEA</a></li>
+                                    <li><a href="" target="_blank"><i class="fa fa-angle-right"></i>Temas</a></li>
+                                    <li><a href="http://portal.ipea.gov.br/radar/radar-em-pdf" target="_blank"><i class="fa fa-angle-right"></i>Radar PDF</a></li>
+                                </ul>
+                                
+                            </div>
+                            
+                            <div class="col-md-4 col-contatos">
+                                
+                                <h4>CONTATOS</h4>
+                                
+                                <address>
+                                    <i class="fa fa-map-marker"></i> <span>Ipea - Brasília <small>SBS - Quadra 1 - Bloco J - Ed. BNDES 70076-900 - Brasília - DF - Brasil</small></span>
+                                </address>
+                                
+                                <p><a href="06120265422"><i class="fa fa-phone"></i>(61)2026-5422</a></p>
+                                
+                                <ul class="list-unstyled list-socials">
+                                    
+                                    <li>
+                                        <a href="https://www.facebook.com/IPEA1964" target="_blank"><i class="fa fa-facebook"></i></a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="https://twitter.com/ipeaonline" target="_blank"><i class="fa fa-twitter"></i></a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="https://www.youtube.com/user/agenciaipea" target="_blank"><i class="fa fa-youtube"></i></a>
+                                    </li>
+                                    
+                                </ul>
+                                
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            
+            <div class="row row-caribbean_green">
+                
+                <div class="container">
+                    
+                    <p class="center-block">Copyright © 2017 Sites Ipea. Todos os direitos reservados.</p>
+                    
+                </div>
+                
+            </div>
+            
+        </footer>
+        
+        <script src="../lib/jquery/jquery.min.js"></script>
+        <script src="../lib/owl.carousel/owl.carousel.min.js"></script>
+        <script src="../lib/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../lib/js/efeitos.js"></script>
+        
+    </body>
+</html>
