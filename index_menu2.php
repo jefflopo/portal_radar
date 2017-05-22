e1133ed8f70d3bf957ee2f8f435ae10ee74a6f4d
jefflopo <jeffersonlopo@gmail.com>
2017-05-22 09:41:20 -0300

    Inseridas rotas para cada pagina .php

--- index_menu2.php
+++ index_menu2.php
@@ -30,6 +30,22 @@ $app->get('/', function ($request, $response, $args) {
     require_once ("view/index_menu2.php");
 });
 
+$app->get('/agricultura', function ($request, $response, $args) {
+    require_once ("view/agricultura.php");
+});
+
+$app->get('/ciencia_tecnologia', function ($request, $response, $args) {
+    require_once ("view/ciencia_tecnologia.php");
+});
+
+$app->get('/radar', function ($request, $response, $args) {
+    require_once ("view/radarPDF.php");
+});
+
+$app->get('/temas', function ($request, $response, $args) {
+    require_once ("view/temasRadar.php");
+});
+
 $app->get('/hello[/{name}]', function ($request, $response, $args) {
     $response->write("Hello, " . $args['name']);
     return $response;
