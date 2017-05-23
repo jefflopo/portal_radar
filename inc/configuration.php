3f2a3bae4ebf3e0aef1fffa8da602cc1073e9faf
jefflopo <jeffersonlopo@gmail.com>
2017-05-23 12:38:38 -0300

    Implementado a funcao select para encodar em utf8

--- inc/configuration.php
+++ inc/configuration.php
@@ -15,6 +15,25 @@
             
         }
         
+        public function select($string_query) {
+            $result = $this->query($string_query);
+            
+            $data = array();
+
+            while( $row = mysqli_fetch_array($result) ){
+                
+                foreach ($row as $key=>$value){
+                    $row[$key] = utf8_encode($value);
+                }
+                
+                array_push($data, $row);
+            }
+            
+            unset($result);
+            
+            return $data;
+        }
+        
         public function __destruct(){
             
             mysqli_close($this->conn);
